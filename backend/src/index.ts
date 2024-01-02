c273ff5e66eba3b82be9de997aeb8cd329b636e5
Sandrine Cipolla <sandrine.cipolla@gmail.com>
2024-01-03 00:05:02 +0100

    add cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import connection from "./db";
 import stockRoutes from "./routes/stockRoutes";
 
@@ -15,10 +16,28 @@ connection.connect((err) => {
   console.log("Connexion à la base de données établie avec succès");
 });
 
+// Utiliser CORS middleware
+app.use(cors());
+
+// Middleware pour analyser les corps de requête en JSON
+app.use(express.json());
+
 // Utiliser les routes définies dans stockRoutes.ts
 app.use("/api/v1", stockRoutes);
+
+// Gestion des erreurs 404
+app.use((req, res, next) => {
+  res.status(404).send("Route non trouvée");
+});
+
+// Gestion des erreurs globales
+app.use((err: Error, req: express.Request, res: express.Response, next: express.NextFunction): void => {
+  console.error(err.stack);
+  res.status(500).send('Erreur interne du serveur');
+});
 
 // Démarrer le serveur
 app.listen(port, () => {
   console.log(`Serveur backend en cours d'exécution sur le port ${port}`);
 });
+
