da9e3a5f8c9895eae44be7fc578b829e19a66166
Sandrine Cipolla <sandrine.cipolla@gmail.com>
2024-02-14 15:16:00 +0100

    Updating error messages and mocks in stock routes and tests

--- backend/src/utils/dbUtils.ts
+++ backend/src/utils/dbUtils.ts
@@ -1,9 +1,9 @@
-import {PoolConnection} from "mysql2/promise";
-import {newStocks, Stock} from "../../tests/__mocks__/mockedData";
+import { PoolConnection } from "mysql2/promise";
+import { newStocks, Stock } from "../../tests/__mocks__/mockedData";
 
 export interface TableColumn {
-    column_name: string;
-    data_type: string;
+  column_name: string;
+  data_type: string;
 }
 
 // export async function getTableStructure(connection: PoolConnection, tableName: string): Promise<TableColumn[]> {
@@ -26,45 +26,48 @@ export interface TableColumn {
 //         throw new Error(`Erreur lors de la récupération de la structure de la table : ${error.message}`);
 //     }
 // }
-export async function getTableStructure(connection: PoolConnection, tableName: string): Promise<TableColumn[]> {
-    try {
-        console.log('Fetching table structure for table:', tableName);
+export async function getTableStructure(
+  connection: PoolConnection,
+  tableName: string
+): Promise<TableColumn[]> {
+  try {
+    console.log("Fetching table structure for table:", tableName);
 
-        const query = `
+    const query = `
             SELECT column_name, data_type
             FROM information_schema.columns
             WHERE table_name = ?
         `;
-        console.log('Executing query:', query);
+    console.log("Executing query:", query);
 
-        // const result = await connection.query(query, [tableName]);
-
-
-        const [rows] =  await connection.query(query, [tableName]);
-        console.log('rows', rows);
-        if (!rows) {
-            throw new Error(`Aucune donnée retournée pour la table ${tableName}`);
-        }
-        return rows as TableColumn[];
-    } catch (error: any) {
-        throw new Error(`Erreur lors de la récupération de la structure de la table : ${error.message}`);
+    const [rows] = await connection.query(query, [tableName]);
+    console.log("rows", rows);
+    if (!rows) {
+    throw new Error(`No data returned for table ${tableName}`);
     }
+    return rows as TableColumn[];
+  } catch (error: any) {
+    throw new Error(
+        //Erreur lors de la récupération de la structure de la table
+      `Error retrieving table structure : ${error.message}`
+    );
+  }
 }
 
-
-
-
-export async function insertStock(connection: PoolConnection, stocks: Stock[]): Promise<void> {
-    try {
-        console.log("Inserting stocks:", stocks);
-        const values = stocks.map(stock => [stock.id, stock.label]);
-        console.log("Formatted values:", values);
-        const query = 'INSERT INTO stocks (id, label) VALUES ?';
-        console.log("query",query)
-        await connection.query(query, [values]);
-        console.log("Stocks inserted successfully!");
-    } catch (error: any) {
-        console.error(`Error inserting stocks: ${error.message}`);
-        throw new Error(`Erreur lors de l'insertion des stocks : ${error.message}`);
-    }
+export async function insertStock(
+  connection: PoolConnection,
+  stocks: Stock[]
+): Promise<void> {
+  try {
+    console.log("Inserting stocks:", stocks);
+    const values = stocks.map((stock) => [stock.id, stock.label]);
+    console.log("Formatted values:", values);
+    const query = "INSERT INTO stocks (id, label) VALUES ?";
+    console.log("query", query);
+    await connection.query(query, [values]);
+    console.log("Stocks inserted successfully!");
+  } catch (error: any) {
+    console.error(`Error inserting stocks: ${error.message}`);
+    throw new Error(`Error inserting stocks : ${error.message}`);
+  }
 }
--- backend/tests/integration/db.test.ts
+++ backend/tests/integration/db.test.ts
@@ -67,4 +67,3 @@ describe("Server setup", () => {
     }
   });
 });
-
