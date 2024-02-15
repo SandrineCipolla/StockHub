2ff0d7165e2abd320451f6f4c903fc79c985b35e
Sandrine Cipolla <sandrine.cipolla@gmail.com>
2024-02-15 12:10:35 +0100

    add test for insertStock()

--- backend/tests/unit/stock.test.ts
+++ backend/tests/unit/stock.test.ts
@@ -9,10 +9,7 @@ import {
   fakeStocksWithoutId,
   newStocks,
 } from "../__mocks__/mockedData";
-import {
-  getAllStocks,
-
-} from "../../src/controllers/stockController";
+import { getAllStocks } from "../../src/controllers/stockController";
 import { Request, Response } from "express";
 import { getTableStructure, insertStock } from "../../src/utils/dbUtils";
 
@@ -58,7 +55,6 @@ describe("getAllStocks", () => {
       expect(res.json).toHaveBeenCalledWith(fakeStocks);
     });
   });
-
 
   describe("when a new stock is created in an empty stock Table", () => {
     it("should insert a new entry to the empty stock stock table", async () => {
@@ -139,6 +135,51 @@ describe("getAllStocks", () => {
       if (finalStocks[3]) {
         expect((finalStocks[3] as any).label).toEqual("MyNewStock");
       }
+    });
+  });
+});
+
+describe("insertStock", () => {
+  let mockConnection: PoolConnection;
+
+  beforeEach(() => {
+    mockConnection = {
+      query: jest.fn(),
+    } as unknown as PoolConnection;
+  });
+
+  afterEach(() => {
+    jest.restoreAllMocks();
+  });
+
+  describe("when a new stock is created", () => {
+    it("should insert a new entry to the stock table", async () => {
+      //  Spécification de la valeur avant l'insertion
+      (mockConnection.query as jest.Mock).mockResolvedValueOnce([...fakeStocks]);// ? ou [fakeStocks] ?
+     
+
+      // nouvelle entrée
+      await insertStock(mockConnection, newStocks);
+
+      // Spécification de la valeur retour après l'insertion
+      (mockConnection.query as jest.Mock).mockResolvedValueOnce([
+        ...fakeStocks,
+        ...newStocks,
+      ]);
+
+    
+      //  contenu de la table après l'insertion
+      const finalStocks = (await mockConnection.query(
+        "SELECT * FROM stocks"
+      )) as RowDataPacket[][];
+      console.log("finalStocks", finalStocks);
+
+      // Vérification que la requête d'insertion a été appelée avec les bonnes valeurs
+      expect(mockConnection.query).toHaveBeenCalledWith(
+        "INSERT INTO stocks (id, label) VALUES ?",
+        [newStocks.map((stock) => [stock.id, stock.label])]
+      );
+      expect(finalStocks).toEqual([...fakeStocks, ...newStocks]);
     });
   });
 });
