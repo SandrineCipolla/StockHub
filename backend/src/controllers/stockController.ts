7ee10f02cba3e500d4c8f368e44cbce7e6110165
Sandrine Cipolla <sandrine.cipolla@gmail.com>
2024-03-27 21:20:04 +0100

    update controller with status code

--- backend/src/controllers/stockController.ts
+++ backend/src/controllers/stockController.ts
@@ -13,18 +13,15 @@ export const getAllStocks = async (
             FieldPacket[]
         ];
 
-        if (res && res.json) {
-
-            res.json(stocks);
+        if (res) {
+            res.status(200).json(stocks);
         } else {
-            throw new Error(
-                "Response or res.json is undefined. Cannot call res.status and res.json for error handling."
-            );
+            throw new Error("Response is undefined. Cannot call res.status and res.json for error handling.");
         }
     } catch (err: any) {
         console.error(err); // Enregistrer l'erreur
-        if (res && res.json) {
-            res.json({error: err.message}); // Envoyer une réponse d'erreur au client
+        if (res) {
+            res.status(500).json({error: err.message}); // Envoyer une réponse d'erreur au client
         }
         throw err; // Rejeter l'erreur pour que le test puisse la détecter
     }
