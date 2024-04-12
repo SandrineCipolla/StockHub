2b0678302a6ca7304cce90fb2af0f18cb22d52dd
SancCi <100938864+SandrineCipolla@users.noreply.github.com>
2024-04-12 16:19:56 +0200

    add models file for interface in thr front

---
+++ frontend/src/models.ts
@@ -0,0 +1,11 @@
+export interface Stock {
+    ID: number;
+    LABEL: string;
+}
+
+export interface StockDetail {
+    ID: number;
+    LABEL: string;
+    QUANTITY: number;
+    DESCRIPTION: string;
+}
