9749e90f88775088bd05d2ca8e1a743b67ec208e
SancCi <100938864+SandrineCipolla@users.noreply.github.com>
2024-04-30 12:02:46 +0200

    created dist folder to use http-server (prod)

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,12 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import * as path from "node:path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      input: 'src/main.tsx',
+      input: path.resolve(__dirname, 'index.html'),
     },
   },
 })
