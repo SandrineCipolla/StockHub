cf5454eb54828183d7df19062c0dafd15cf1aea1
SancCi <100938864+SandrineCipolla@users.noreply.github.com>
2024-08-12 12:12:47 +0200

    moved sql scripts from stockController to readStockRepository or writeStockRepository

--- backend/src/repositories/readStockRepository.ts
+++ backend/src/repositories/readStockRepository.ts
@@ -3,15 +3,29 @@ import {FieldPacket, PoolConnection, RowDataPacket} from "mysql2/promise";
 export const readAllStocks = async (
     connection: PoolConnection
 ) => {
-    try {
-        const stocks = (await connection.query("SELECT * FROM stocks")) as [
-            RowDataPacket[],
-            FieldPacket[]
-        ];
-        return stocks[0]
-    }
-    catch(err :any) {
-        console.log('Error occured when retrieving all stocks', err);
-        throw err;
-    }
-}
+    const stocks = (await connection.query("SELECT * FROM stocks")) as [
+        RowDataPacket[],
+        FieldPacket[]
+    ];
+    return stocks[0]
+};
+
+export const readStockDetails = async (connection: PoolConnection, ID: number) => {
+    const [stock] = (await connection.query("SELECT * FROM stocks WHERE ID = ?", [ID])) as [RowDataPacket[], FieldPacket[]];
+    return stock;
+};
+
+export const readStockItems = async (connection: PoolConnection, ID: number) => {
+    const [items] = (await connection.query("SELECT * FROM items WHERE STOCK_ID = ?", [ID])) as [RowDataPacket[], FieldPacket[]];
+    return items;
+};
+
+export const readAllItems = async (connection: PoolConnection) => {
+    const [items] = (await connection.query("SELECT * FROM items")) as [RowDataPacket[], FieldPacket[]];
+    return items;
+};
+
+export const readItemDetails = async (connection: PoolConnection, itemID: number) => {
+    const [items] = (await connection.query("SELECT * FROM items WHERE ID = ?", [itemID])) as [RowDataPacket[], FieldPacket[]];
+    return items;
+};
