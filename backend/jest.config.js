0d06989b837bf8d5fa0763943e02b8bbcd962f0a
SancCi <100938864+SandrineCipolla@users.noreply.github.com>
2024-09-06 09:21:43 +0200

    updated tsconfig according ide advise

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -1,15 +1,12 @@
-module.exports =   {
+module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'node',
     moduleFileExtensions: ['ts', 'tsx', 'js', 'json'],
     transform: {
-        '^.+\\.(ts|tsx)$': 'ts-jest',
+        '^.+\\.(ts|tsx)$': ['ts-jest', {
+            tsconfig: 'tsconfig.json',
+        }],
     },
     setupFilesAfterEnv: ['<rootDir>/tests/setupTests.ts'],
     testMatch: ['<rootDir>/tests/**/*.test.(ts|tsx)'],
-    globals: {
-        'ts-jest': {
-        tsconfig: 'tsconfig.json',
-        },
-    },
 };
