a1a686461b9ce40c9dd6aa90ee201588529b327e
SancCi <100938864+SandrineCipolla@users.noreply.github.com>
2024-09-12 13:12:40 +0200

    Feature/added coverage ci config (#31)
    * added config for backend
    
    * added config for frontend
    
    * updated config github actions
    
    * updated config github actions
    
    * updated jest.ci.config in back

--- backend/jest.ci.config.js
+++ backend/jest.ci.config.js
@@ -11,4 +11,13 @@ module.exports = {
     setupFiles: ['dotenv/config'],
     testMatch: ['<rootDir>/tests/**/*.test.(ts|tsx)'],
     testPathIgnorePatterns: ['<rootDir>/tests/integration'],
+
+    // Coverage
+    collectCoverage: process.env.COVERAGE === 'true', // Active la couverture seulement si une variable d'environnement est définie
+    coverageDirectory: './coverage',
+    collectCoverageFrom: [
+        '<rootDir>/src/**/*.ts', // Inclure tous les fichiers TypeScript dans `src`
+        '!<rootDir>/tests/**', // Exclure les tests d'intégration
+        '!<rootDir>/src/**/*.d.ts', // Exclure les fichiers de types (déclaration .d.ts)
+    ],
 };
