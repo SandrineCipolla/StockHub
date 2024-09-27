0bc7c16e4cb838cf3023ae522eab7e17bddc5372
SancCi <100938864+SandrineCipolla@users.noreply.github.com>
2024-09-27 10:44:13 +0200

    updating code according review and merge

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import {BrowserRouter as Router, Navigate, Route, Routes} from "react-router-dom";
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import StocksList from "./components/StocksList";
@@ -9,6 +9,9 @@ import {AuthenticationResult, EventType} from "@azure/msal-browser";
 import {b2cPolicies, protectedResources} from "./authConfig.ts";
 
 import {useMsal} from "@azure/msal-react";
+import StockDetailsWithItems from "./components/StockDetailsWithItems.tsx";
+import ItemsList from "./components/ItemsList.tsx";
+import ItemDetails from "./components/ItemDetails.tsx";
 
 function ProtectedComponent() {
     const {instance} = useMsal();
@@ -100,8 +103,8 @@ function ProtectedComponent() {
                 <Header/>
                 <main>
                     <Routes>
-                       /* <Route path="/" element={<Navigate to="/home"/>}/>*/
-                        <Route path="/home" element={<Home/>}/>
+                        {/*<Route path="/" element={<Navigate to="/home"/>}/>*/}
+                        <Route path="/" element={<Home/>}/>
                         <Route path="/stocks" element={<StocksList/>}/>
                         <Route path="/items" element={<ItemsList/>}/>
                         <Route path="/stocks/:ID" element={<StockDetailsWithItems/>}/>
