2b65e9f35772979a161f1a2d1bce92a58987d000
SancCi <100938864+SandrineCipolla@users.noreply.github.com>
2024-11-13 00:43:21 +0100

    including node externals.

--- backend/webpack.config.js
+++ backend/webpack.config.js
@@ -1,10 +1,12 @@
 const path = require('path');
 const nodeExternals = require('webpack-node-externals');
 
+//    externals: [nodeExternals()], // Exclut `node_modules` du bundle
+
 module.exports = {
     entry: './src/index.ts', // Point d'entrée de votre application
     target: 'node', // Spécifie que le bundle est pour Node.js, pas pour le navigateur
-    externals: [nodeExternals()], // Exclut `node_modules` du bundle
+
     output: {
         path: path.resolve(__dirname, 'package'),
         filename: 'server.js',
